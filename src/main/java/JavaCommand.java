a0f3003fef4754a7ff899fc863a0c2e7238f16d0
jungfer <meikjungfer@hotmail.de>
2020-06-09 14:17:10 +0200

    Add Runtime Java compiler command

---
+++ src/main/java/JavaCommand.java
@@ -0,0 +1,27 @@
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import javax.annotation.Nonnull;
+
+public class JavaCommand  extends Command {
+    @Override
+    public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+        User user = event.getAuthor();
+        if (user.isBot())
+            return;
+
+        Message message = event.getMessage();
+        MessageChannel channel = event.getChannel();
+
+        String msg = message.getContentDisplay();
+        String[] params = msg.split(" ");
+        String command = params[0];
+
+        if (event.isFromType(ChannelType.TEXT)) {
+
+        }
+    }
+}
